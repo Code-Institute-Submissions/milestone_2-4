4de11be1cdcc4620e2a7d5b1f89895fa8c055c25
Stephen Keogh <stevokeogh@hotmail.com>
2020-09-19 14:51:59 +0000

    tested click events on all buttons- fixed a few error in code

--- stephen.js
+++ stephen.js
@@ -14,10 +14,38 @@ let win ;
 
 const turnCounter = document.querySelector("#turn");
 const topLeft = document.querySelector("#topLeft");
-const topRight = document.querySelector("#topRight")
-const bottomRight = document.querySelector("bottomRight");
+const topRight = document.querySelector("#topRight");
+const bottomLeft = document.querySelector("#bottomLeft");
+const bottomRight = document.querySelector("#bottomRight");
 const difficultyButton = document.querySelector("#difficulty");
 const powerButton = document.querySelector("#power");
 const startButton = document.querySelector("#start");
 
+difficultyButton.addEventListener('click', (event) => {
+    console.log("test");
+})
+
+
+topLeft.addEventListener('click', (event) => {
+    console.log("test TL");
+})
+
+topRight.addEventListener('click', (event) => {
+    console.log("test TR");
+})
+
+bottomRight.addEventListener('click', (event) => {
+    console.log("test BR");
+})
+bottomLeft.addEventListener('click', (event) => {
+    console.log("test BL");
+})
+
+powerButton.addEventListener('click', (event) => {
+    console.log("test power");
+})
+
+startButton.addEventListener('click', (event) => {
+    console.log("test start");
+})
 
