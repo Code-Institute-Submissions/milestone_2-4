fcd960885a73e50131e0015f87bb227047447c0b
Stephen Keogh <stevokeogh@hotmail.com>
2020-09-19 14:31:11 +0000

    added IDs to HTML, Created interative and non interactive variables in JS file

---
+++ stephen.js
@@ -0,0 +1,23 @@
+
+
+
+let order = [];          // intialising an Array for sequences and declaring variables for different states of the game.
+let playerOrder = []      
+let flash;               
+let turn;
+let correct;         
+let cpuTurn;
+let difficult = false;  
+let power = false;
+let win ; 
+
+
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topLeft");
+const topRight = document.querySelector("#topRight")
+const bottomRight = document.querySelector("bottomRight");
+const difficultyButton = document.querySelector("#difficulty");
+const powerButton = document.querySelector("#power");
+const startButton = document.querySelector("#start");
+
+
